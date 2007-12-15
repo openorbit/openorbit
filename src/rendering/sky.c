c32316befd2ed26c0d4a03b89adf5ad4659aed72
hannibal.holm <hannibal.holm@67975e55-dd32-0410-8bc3-a9813c38a78a>
2007-12-15 20:27:07 +0000

    Cleanup of license blocks
    Astetic cleanup of a few license blocks.
    
    
    git-svn-id: https://openorbit.googlecode.com/svn/trunk@60 67975e55-dd32-0410-8bc3-a9813c38a78a

--- src/rendering/sky.c
+++ src/rendering/sky.c
@@ -25,7 +25,7 @@
     of those above. If you wish to allow use of your version of this file only
     under the terms of the GPL or the LGPL and not to allow others to use your
     version of this file under the MPL, indicate your decision by deleting the
-    provisions above and replace  them with the notice and other provisions
+    provisions above and replace them with the notice and other provisions
     required by the GPL or the LGPL.  If you do not delete the provisions
     above, a recipient may use your version of this file under either the MPL,
     the GPL or the LGPL."
