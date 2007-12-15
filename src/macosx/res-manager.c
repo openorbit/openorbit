ee10b15ce5c7d250d98bb33ac76d3e39bef40f48
hannibal.holm <hannibal.holm@67975e55-dd32-0410-8bc3-a9813c38a78a>
2007-12-15 20:20:08 +0000

    Initial resource manager functions for MacOS X
    The resource manager will locate and open resource files used by the
    application. The files in this checkin are Mac specific, allowing us to bundle
    resources in the application bundle.
    
    Ideally these should be implemented for generic POSIX and WIN32 as well. For the
    moment this is down prioritised as the main goal at the moment is to do
    sufficient clean-up in order to do a first public release of the source code.
    
    
    git-svn-id: https://openorbit.googlecode.com/svn/trunk@46 67975e55-dd32-0410-8bc3-a9813c38a78a

---
+++ src/macosx/res-manager.c
@@ -0,0 +1,134 @@
+/* 
+    The contents of this file are subject to the Mozilla Public License
+    Version 1.1 (the "License"); you may not use this file except in compliance
+    with the License. You may obtain a copy of the License at
+    http://www.mozilla.org/MPL/
+
+    Software distributed under the License is distributed on an "AS IS" basis,
+    WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+    for the specific language governing rights and limitations under the
+    License.
+
+    The Original Code is the Open Orbit space flight simulator.
+
+    The Initial Developer of the Original Code is Mattias Holm. Portions
+    created by the Initial Developer are Copyright (C) 2006 the
+    Initial Developer. All Rights Reserved.
+
+    Contributor(s):
+        Mattias Holm <mattias.holm(at)contra.nu>.
+
+    Alternatively, the contents of this file may be used under the terms of
+    either the GNU General Public License Version 2 or later (the "GPL"), or
+    the GNU Lesser General Public License Version 2.1 or later (the "LGPL"), in
+    which case the provisions of GPL or the LGPL License are applicable instead
+    of those above. If you wish to allow use of your version of this file only
+    under the terms of the GPL or the LGPL and not to allow others to use your
+    version of this file under the MPL, indicate your decision by deleting the
+    provisions above and replace  them with the notice and other provisions
+    required by the GPL or the LGPL.  If you do not delete the provisions
+    above, a recipient may use your version of this file under either the MPL,
+    the GPL or the LGPL."
+ */
+
+#include <CoreFoundation/CoreFoundation.h>
+#include "res-manager.h"
+
+// TODO: More thurough error checking and handling
+char*
+res_get_path(const char *file_name)
+{
+    assert(file_name != NULL);
+    
+    
+    char *end = strrchr(file_name, '\0');
+    char *last_dot = strrchr(file_name, '.');
+    char *last_slash = strrchr(file_name, '/');
+    
+    if (! last_dot) {
+        return NULL;
+    }
+
+    const char *fileBase;
+    const char *fileType;
+    const char *fileSubDir;
+
+    if (last_slash) {
+        fileBase = malloc(last_dot - last_slash + 1);
+        fileType = malloc(end - last_dot + 1);
+        fileSubDir = malloc(last_slash - file_name + 1);
+    } else {
+        fileBase = malloc(last_dot - file_name + 1);
+        fileType = malloc(end - last_dot + 1);
+        fileSubDir = NULL;
+    }
+    
+
+    CFBundleRef bundle = CFBundleGetMainBundle();
+    if (bundle == NULL) return NULL;
+    
+    CFStringRef resName = CFStringCreateWithCString(kCFAllocatorDefault,
+                                                    fileBase,
+                                                    kCFStringEncodingUTF8);
+
+    CFStringRef resType = CFStringCreateWithCString(kCFAllocatorDefault,
+                                                    fileType,
+                                                    kCFStringEncodingUTF8);
+
+    CFStringRef resSubDir;
+    if (fileSubDir != NULL) {
+        resSubDir = CFStringCreateWithCString(kCFAllocatorDefault,
+                                              fileSubDir,
+                                              kCFStringEncodingUTF8);
+    } else {
+        resSubDir = NULL;
+    }
+    
+    CFURLRef url = CFBundleCopyResourceURL(bundle, resName, resType, resSubDir);
+    
+    UInt8 path = malloc(MAX_PATH_LEN+1);
+    // true if absolute path is to be used...
+    if (!CFURLGetFileSystemRepresentation(url, false, path, MAX_PATH_LEN) {
+        free(path);
+        path = NULL;
+    }
+    
+    free(fileBase);
+    free(fileType);
+    free(fileSubDir);
+    CFRelease(resName);
+    CFRelease(resType);
+    CFRelease(resSubDir);
+    CFRelease(url);
+    
+    return path;
+}
+
+FILE*
+res_get_file(const char *file_name)
+{
+    char *path = res_get_path(file_name);
+    
+    if (path != NULL) {
+        FILE *file = fopen(path, "r");
+        free(path);
+        return file;
+    }
+
+    return NULL;
+}
+
+int
+res_get_fd(const char *file_name)
+{
+    char *path = res_get_path(file_name);
+    
+    if (path != NULL) {
+        int fd = open(path, O_RDONLY);
+        free(path);
+        
+        return fd;
+    }
+
+    return -1;
+}
