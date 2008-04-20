4e07e13ea0ee188c829f5e555b0f49363e984f6c
hannibal.holm <hannibal.holm@67975e55-dd32-0410-8bc3-a9813c38a78a>
2008-04-20 16:54:55 +0000

    Update to cmake 2.6
    The files have been updated for cmake 2.6, the archflags file have been
    re-factored a bit and the system is being prepared for a config file, instead of
    setting compile flags.
    
    
    git-svn-id: https://openorbit.googlecode.com/svn/trunk@107 67975e55-dd32-0410-8bc3-a9813c38a78a

--- cmake/arch-flags.cmake
+++ cmake/arch-flags.cmake
@@ -1,22 +1,27 @@
 # Default flags
 set(CMAKE_C_FLAGS_DEBUG "-g -std=c99")
-set(CMAKE_C_FLAGS_RELEASE "-std=c99")
+set(CMAKE_C_FLAGS_RELEASE "-std=c99 -O2")
 
+set(ARCH_FLAGS_DEBUG "")
+set(ARCH_FLAGS_RELEASE "")
 
 if (ENABLE_VECTORISE)
     include(CheckCCompilerFlag)
-    check_c_compiler_flag(-faltivec ALTIVEC_AVAILABLE)
-    check_c_compiler_flag(-msse3 SSE3_AVAILABLE)
+    check_c_compiler_flag(-faltivec HAVE_ALTIVEC)
+    check_c_compiler_flag(-msse3 HAVE_SSE3)
 
-    if (ALTIVEC_AVAILABLE)
-        add_definitions(-DHAVE_ALTIVEC)
-        set(CMAKE_C_FLAGS_RELEASE "-std=c99 -O2 -faltivec")
-        set(CMAKE_C_FLAGS_DEBUG "-g -std=c99 -faltivec")
-    endif (ALTIVEC_AVAILABLE)
-    
-    if (SSE3_AVAILABLE)
-        add_definitions(-DHAVE_SSE3)
-    endif (SSE3_AVAILABLE)
-    
-    add_definitions(-DENABLE_VECTORISE)
+    if (HAVE_ALTIVEC)
+        set(ARCH_FLAGS_RELEASE "${ARCH_FLAGS_RELEASE} -faltivec")
+        set(ARCH_FLAGS_DEBUG "${ARCH_FLAGS_DEBUG} -faltivec")
+    endif (HAVE_ALTIVEC)
+
+    if (HAVE_SSE3)
+        set(ARCH_FLAGS_RELEASE "${ARCH_FLAGS_RELEASE} -msse3")
+        set(ARCH_C_FLAGS_DEBUG "${ARCH_FLAGS_DEBUG} -msse3")
+    endif (HAVE_SSE3)
+	
 endif (ENABLE_VECTORISE)
+
+
+set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} ${ARCH_FLAGS_DEBUG}")
+set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} ${ARCH_FLAGS_RELEASE}")
