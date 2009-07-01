fc4d6af4d81b088c3048e77f2a69e044a62d130a
Mattias Holm <mattias.holm@openorbit.org>
2009-07-01 19:00:49 +0200

    Bugfix for viewport settings
    The viewport was set to be in the height of an
    un-initialised value. This has hereby been rectified and
    the drawing of the night sky is now again working.

--- src/rendering/render.c
+++ src/rendering/render.c
@@ -87,12 +87,12 @@ init_gl(void)
     
     ooConfGetFloatDef("openorbit/video/gl/fovy", &fovy, 45.0f);
     ooConfGetFloatDef("openorbit/video/gl/aspect", &aspect, 1.33f);
-        
+
     gluPerspective(fovy, aspect, /*near*/0.001, /*far*/100.0);
     
     ooConfGetIntDef("openorbit/video/width", &width, 640);
-    ooConfGetIntDef("openorbit/video/height", &width, 480);
-    
+    ooConfGetIntDef("openorbit/video/height", &height, 480);
+
     glViewport(0, 0, width, height);
 }
 
