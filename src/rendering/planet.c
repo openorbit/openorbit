5a63cc81787467bef0bffced11d876f72b7f5e11
Mattias Holm <mattias.holm@openorbit.org>
2009-07-15 10:10:10 +0200

    Relicensing to LGPLv3
    In order to reduce confusion, the MPL will no longer be applicable to Open Orbit. Open Orbit was previously
    triple licensed under the MPL, GPL and LGPL. It has now
    moved to the LGPLv3 permanently.
    
    This will effectively protect Open Orbit while still
    allowing plug-ins to be distributed in whatever licence
    that the plugin-developer sees fit.

--- src/rendering/planet.c
+++ src/rendering/planet.c
@@ -1,35 +1,21 @@
-/* 
- The contents of this file are subject to the Mozilla Public License
- Version 1.1 (the "License"); you may not use this file except in compliance
- with the License. You may obtain a copy of the License at
- http://www.mozilla.org/MPL/
- 
- Software distributed under the License is distributed on an "AS IS" basis,
- WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- for the specific language governing rights and limitations under the
- License.
- 
- The Original Code is the Open Orbit space flight simulator.
- 
- The Initial Developer of the Original Code is Mattias Holm. Portions
- created by the Initial Developer are Copyright (C) 2007 the
- Initial Developer. All Rights Reserved.
- 
- Contributor(s):
- Mattias Holm <mattias.holm(at)contra.nu>.
- 
- Alternatively, the contents of this file may be used under the terms of
- either the GNU General Public License Version 2 or later (the "GPL"), or
- the GNU Lesser General Public License Version 2.1 or later (the "LGPL"), in
- which case the provisions of GPL or the LGPL License are applicable instead
- of those above. If you wish to allow use of your version of this file only
- under the terms of the GPL or the LGPL and not to allow others to use your
- version of this file under the MPL, indicate your decision by deleting the
- provisions above and replace them with the notice and other provisions
- required by the GPL or the LGPL. If you do not delete the provisions
- above, a recipient may use your version of this file under either the MPL,
- the GPL or the LGPL."
- */
+/*
+  Copyright 2007 Mattias Holm <mattias.holm(at)openorbit.org>
+
+  This file is part of Open Orbit.
+
+  Open Orbit is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Open Orbit is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with Open Orbit.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "planet.h"
 #include "texture.h"
--- src/vmath/vmath-matvec.inl
+++ src/vmath/vmath-matvec.inl
@@ -1,3 +1,22 @@
+/*
+  Copyright 2008 Mattias Holm <mattias.holm(at)openorbit.org>
+
+  This file is part of Open Orbit.
+
+  Open Orbit is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Open Orbit is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with Open Orbit.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /* Note that this is an umbrella file as well as a default definition file, there
 might be more architectural specialised files further down from here
 */
