66a4823d7175c27d676969e2346057a3c109f651
Mattias Holm <mattias.holm@openorbit.org>
2009-10-22 22:41:53 +0200

    Minor indentation cleanup

--- src/rendering/planet.c
+++ src/rendering/planet.c
@@ -27,16 +27,16 @@
 void ooPlanetDraw(OOplanet *planet)
 {
 //  if (planet->)
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, planet->texId);
+  glEnable(GL_TEXTURE_2D);
+  glBindTexture(GL_TEXTURE_2D, planet->texId);
 
   //  glMatrixMode(GL_MODELVIEW);
-    glEnable(GL_CULL_FACE);
-    glFrontFace(GL_CCW);
-    glColor3f(1.0f, 1.0f, 1.0f);
-    gluSphere(planet->quadratic, planet->radius, 128, 128);
+  glEnable(GL_CULL_FACE);
+  glFrontFace(GL_CCW);
+  glColor3f(1.0f, 1.0f, 1.0f);
+  gluSphere(planet->quadratic, planet->radius, 128, 128);
 
-    glMatrixMode(GL_MODELVIEW);
+  glMatrixMode(GL_MODELVIEW);
 
   // Switch rendering options based on distance to camera
   // Get 
@@ -53,49 +53,46 @@ planet_t gPlanets[MAX_PLANETS];
 int
 planet_add(float x, float y, float z, float radius, float mass, const char *tex_key)
 {
-    if (planet_count >= MAX_PLANETS) {
-        return -1;
-    }
-    
-    gPlanets[planet_count].texid = ooTexNum(tex_key);
-    glBindTexture(GL_TEXTURE_2D, gPlanets[planet_count].texid);
-    
-    
-    gPlanets[planet_count].quadratic = gluNewQuadric();
-    gluQuadricNormals(gPlanets[planet_count].quadratic, GLU_SMOOTH);
-    gluQuadricTexture(gPlanets[planet_count].quadratic, GL_TRUE);
-    // Assume this will work...
-    vf3_set_x(&gPlanets[planet_count].pos, x);
-    vf3_set_y(&gPlanets[planet_count].pos, y);
-    vf3_set_z(&gPlanets[planet_count].pos, z);
+  if (planet_count >= MAX_PLANETS) {
+    return -1;
+  }
 
-    Q_ROT_X(gPlanets[planet_count].rot_ax, DEG_TO_RAD(-90.0f));
-    Q_ROT_X(gPlanets[planet_count].rot_orig, DEG_TO_RAD(0.0f));
+  gPlanets[planet_count].texid = ooTexNum(tex_key);
+  glBindTexture(GL_TEXTURE_2D, gPlanets[planet_count].texid);
 
-    gPlanets[planet_count].radius = radius;
+  gPlanets[planet_count].quadratic = gluNewQuadric();
+  gluQuadricNormals(gPlanets[planet_count].quadratic, GLU_SMOOTH);
+  gluQuadricTexture(gPlanets[planet_count].quadratic, GL_TRUE);
+  // Assume this will work...
+  gPlanets[planet_count].pos = vf3_set(x, y, z);
+
+  Q_ROT_X(gPlanets[planet_count].rot_ax, DEG_TO_RAD(-90.0f));
+  Q_ROT_X(gPlanets[planet_count].rot_orig, DEG_TO_RAD(0.0f));
+
+  gPlanets[planet_count].radius = radius;
     
-    planet_count ++;
-    return 0;
+  planet_count ++;
+  return 0;
 }
 
 void
 planet_rot_ax(planet_t *planet, float rot_rad)
 {
-    quaternion_t res, rot;
-    Q_ROT_Y(rot, rot_rad);
+  quaternion_t res, rot;
+  Q_ROT_Y(rot, rot_rad);
 
-    res = q_mul(planet->rot_ax, rot);
-    planet->rot_ax = res;
+  res = q_mul(planet->rot_ax, rot);
+  planet->rot_ax = res;
 }
 
 void
 planet_rot_orig(planet_t *planet, float rot_rad)
 {
-    quaternion_t res, rot;
-    Q_ROT_Y(rot, rot_rad);
-    
-    res = q_mul(planet->rot_orig, rot);
-    planet->rot_orig = res;
+  quaternion_t res, rot;
+  Q_ROT_Y(rot, rot_rad);
+
+  res = q_mul(planet->rot_orig, rot);
+  planet->rot_orig = res;
 }
 
 //void
@@ -107,57 +104,56 @@ planet_rot_orig(planet_t *planet, float rot_rad)
 void
 planet_draw(planet_t *planet)
 {
-    matrix_t m;
-    glPushMatrix();
-    
-    glBindTexture(GL_TEXTURE_2D, planet->texid);
-        
-    matrix_t rot_orig;
-    matrix_t rot_ax;
-    q_m_convert(&rot_orig, planet->rot_orig);
-    q_m_convert(&rot_ax, planet->rot_ax);
-    
-    glMultMatrixf((GLfloat*)rot_orig.a);
-    glTranslatef(vf3_x(planet->pos), vf3_y(planet->pos), vf3_z(planet->pos) );
-    glMultMatrixf((GLfloat*)rot_ax.a);
-    
-    gluSphere(planet->quadratic, planet->radius, 64, 64);
-    
-    glPopMatrix();
+  matrix_t m;
+  glPushMatrix();
+
+  glBindTexture(GL_TEXTURE_2D, planet->texid);
+
+  matrix_t rot_orig;
+  matrix_t rot_ax;
+  q_m_convert(&rot_orig, planet->rot_orig);
+  q_m_convert(&rot_ax, planet->rot_ax);
+
+  glMultMatrixf((GLfloat*)rot_orig.a);
+  glTranslatef(vf3_x(planet->pos), vf3_y(planet->pos), vf3_z(planet->pos) );
+  glMultMatrixf((GLfloat*)rot_ax.a);
+
+  gluSphere(planet->quadratic, planet->radius, 64, 64);
+
+  glPopMatrix();
 }
 
 void
 planet_draw_all(void)
 {
-    glEnable(GL_TEXTURE_2D);
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+  glEnable(GL_TEXTURE_2D);
+  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 
-    
-    glPolygonMode(GL_FRONT,GL_FILL);
-    glPolygonMode(GL_BACK,GL_LINE);
+  glPolygonMode(GL_FRONT,GL_FILL);
+  glPolygonMode(GL_BACK,GL_LINE);
 
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-    GLfloat lightCol[] = {1.0,1.0,1.0,1.0};
-    GLfloat pos[] = {0.0, 0.0, 0.0, 1.0};
-    glShadeModel(GL_SMOOTH);
+  glEnable(GL_LIGHTING);
+  glEnable(GL_LIGHT0);
+  GLfloat lightCol[] = {1.0,1.0,1.0,1.0};
+  GLfloat pos[] = {0.0, 0.0, 0.0, 1.0};
+  glShadeModel(GL_SMOOTH);
 
-    //glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightCol);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, lightCol);
-    glLightfv(GL_LIGHT0, GL_POSITION, pos);
-    
-    GLfloat col[] = {1.0f, 1.0f, 1.0f, 0.0f};
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, col);
-    //glMaterialfv(GL_FRONT, GL_SPECULAR, col);    
-    glColor3f(1.0f, 1.0f, 1.0f);
-    
-    for (int i = 1 ; i < planet_count ; i ++) {
-        planet_draw(&gPlanets[i]);
-    }
-    
-    glDisable(GL_LIGHTING);
+  //glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, lightCol);
+  glLightfv(GL_LIGHT0, GL_SPECULAR, lightCol);
+  glLightfv(GL_LIGHT0, GL_POSITION, pos);
 
-    planet_draw(&gPlanets[0]);
-    glDisable(GL_TEXTURE_2D);
+  GLfloat col[] = {1.0f, 1.0f, 1.0f, 0.0f};
+  glMaterialfv(GL_FRONT, GL_DIFFUSE, col);
+  //glMaterialfv(GL_FRONT, GL_SPECULAR, col);
+  glColor3f(1.0f, 1.0f, 1.0f);
+
+  for (int i = 1 ; i < planet_count ; i ++) {
+    planet_draw(&gPlanets[i]);
+  }
+
+  glDisable(GL_LIGHTING);
+
+  planet_draw(&gPlanets[0]);
+  glDisable(GL_TEXTURE_2D);
 }
