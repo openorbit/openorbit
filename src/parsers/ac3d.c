3ba5c73c1f1a33dd368576312ddbe9556d0c84a9
Mattias Holm <mattias.holm@openorbit.org>
2009-11-04 10:19:11 +0100

    AC3D parser works!!!
    The ac3d file loader now successfully parses an
    AC3D file. Though, it may not be complete and
    fully working, and it certainly miss a lot of
    validation checks for ensuring that files are
    correct with respect to the AC3D file format.
    
    Still to fix are the conversion of the file format
    graph into a more suitable internal format for
    rendering.

--- src/parsers/ac3d.c
+++ src/parsers/ac3d.c
@@ -1,18 +1,18 @@
 /*
  Copyright 2009 Mattias Holm <mattias.holm(at)openorbit.org>
- 
+
  This file is part of Open Orbit.
- 
+
  Open Orbit is free software: you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  Open Orbit is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Lesser General Public License for more details.
- 
+
  You should have received a copy of the GNU Lesser General Public License
  along with Open Orbit.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -24,12 +24,14 @@
 #include <stdio.h>
 #include <limits.h>
 #include <string.h>
-
+#include <assert.h>
 #define BUFF_SIZE 1024
 
-#define SCAN_CHECK(bf, str, cnt, ...)                 \
+#define SCAN_CHECK(bf, cnt, str, ...)                 \
   do {                                                \
-    if (sscanf((bf), (str), __VA_ARGS__) != (cnt)) {  \
+    int readCnt = 0;\
+    if ((readCnt = sscanf((bf), (str), __VA_ARGS__)) != (cnt)) {  \
+      fprintf(stderr, "got %d tokens but expected %d from '%s'\n", readCnt, (cnt), (str));\
       goto error;                                     \
     }                                                 \
   } while (0)
@@ -46,7 +48,7 @@ struct ac3d_material_t {
 
 struct ac3d_refline {
   int vert_idx;
-  float tex_x, tex_y; 
+  float tex_x, tex_y;
 };
 struct ac3d_surface_t {
   int tag;
@@ -97,7 +99,7 @@ push_material(struct ac3d_file_t *ac3d, struct ac3d_material_t *mat)
   } else {
     ac3d->materials[ac3d->mat_count ++] = *mat;
     return 0;
-  }  
+  }
 }
 
 #define PUSH_MAT(ac, mt) if (push_material((ac), (mt))) goto error;
@@ -118,7 +120,7 @@ push_object(struct ac3d_file_t *ac3d, struct ac3d_object_t *obj)
   } else {
     ac3d->objs[ac3d->obj_count ++] = obj;
     return 0;
-  }  
+  }
 }
 
 struct ac3d_file_t*
@@ -140,8 +142,10 @@ make_file(void)
 #define FORCE_GETS(bf, sz, fp)        \
   do {                                \
     if (!fgets(buff, BUFF_SIZE, fp)) {\
+      fprintf(stderr, "unexpected end of file, in state %s:%d\n", __FILE__, __LINE__);\
       goto error;                     \
     }                                 \
+    if (buff[strlen(buff)-1] == '\n') buff[strlen(buff)-1] = '\0'; \
   } while (0)
 
 struct ac3d_object_t*
@@ -152,9 +156,9 @@ make_object(void)
       0, NULL, // DATA
       NULL, 0.0, 0.0, //TEXTURE
       { // ROT
-        {0.0, 0.0, 0.0},
-        {0.0, 0.0, 0.0},
-        {0.0, 0.0, 0.0}
+        {1.0, 0.0, 0.0},
+        {0.0, 1.0, 0.0},
+        {0.0, 0.0, 1.0}
       },
       {0.0, 0.0, 0.0}, // POS
       NULL, // URL
@@ -165,7 +169,7 @@ make_object(void)
       0, 0, // num children
       NULL, // children
       NULL // parent
-    };      
+    };
 
   struct ac3d_object_t *newObj = malloc(sizeof(struct ac3d_object_t));
   *newObj = obj;
@@ -175,15 +179,25 @@ make_object(void)
 void
 ac3d_delete(struct ac3d_file_t *ac3d)
 {
-  
+
 }
 
 model_t*
 ac3d_obj_to_model(struct ac3d_object_t *obj)
 {
+  assert(obj != NULL);
   model_t *model = malloc(sizeof(model_t));
+
+  if (obj->num_childs > 10000) {
+    fprintf(stderr, "really huge number of objects\n");
+  }
   model->childCount = obj->num_childs;
-  model->children = calloc(obj->num_childs, sizeof(model_t*));
+
+  if (model->childCount > 0) {
+    model->children = calloc(obj->num_childs, sizeof(model_t*));
+  } else {
+    model->children = NULL;
+  }
 
   for (int i = 0 ; i < obj->num_childs ; ++ i) {
     model->children[i] = ac3d_obj_to_model(obj->children[i]);
@@ -204,6 +218,103 @@ ac3d_to_model(struct ac3d_file_t *ac3d)
   }
 
   return model;
+}
+
+struct ac3d_object_t *
+ac3d_read_obj(FILE *fp, const char *name)
+{
+  char buff[BUFF_SIZE];
+  char nameBuff[BUFF_SIZE];
+
+  struct ac3d_object_t *obj = make_object();
+  obj->kind = strdup(name);
+
+
+  while (!feof(fp)) {
+    FORCE_GETS(buff, BUFF_SIZE, fp);
+    if (!memcmp(buff, "name", 4)) {
+      SCAN_CHECK(buff, 1, "name %s", nameBuff);
+    } else if (!memcmp(buff, "data", 4)) {
+      SCAN_CHECK(buff, 1, "data %d", &obj->data_len);
+    } else if (!memcmp(buff, "texture", 7)) {
+      SCAN_CHECK(buff,  1, "texture %s", nameBuff);
+      obj->texture = strdup(nameBuff);
+    } else if (!memcmp(buff, "texrep", 6)) {
+      SCAN_CHECK(buff, 2, "texrep %f %f", &obj->texrep_x, &obj->texrep_y);
+    } else if (!memcmp(buff, "rot", 3)) {
+      SCAN_CHECK(buff, 9, "rot %f %f %f  %f %f %f  %f %f %f",
+                 &obj->rot[0][0], &obj->rot[0][1], &obj->rot[0][2],
+                 &obj->rot[1][0], &obj->rot[1][1], &obj->rot[1][2],
+                 &obj->rot[2][0], &obj->rot[2][1], &obj->rot[2][2]);
+    } else if (!memcmp(buff, "loc", 3)) {
+      SCAN_CHECK(buff, 3, "loc %f %f %f",
+                 &obj->pos[0], &obj->pos[1], &obj->pos[2]);
+    } else if (!memcmp(buff, "url", 3)) {
+      SCAN_CHECK(buff, 1, "url %s", nameBuff);
+    } else if (!memcmp(buff, "numvert", 7)) {
+      SCAN_CHECK(buff, 1, "numvert %d", &obj->num_verts);
+      obj->verts = calloc(obj->num_verts*3, sizeof(float));
+
+      for (size_t i = 0 ; i < obj->num_verts ; ++ i) {
+        FORCE_GETS(buff, BUFF_SIZE, fp);
+        SCAN_CHECK(buff, 3, "%f %f %f",
+                   &obj->verts[i*3], &obj->verts[i*3+1], &obj->verts[i*3+2]);
+      }
+    } else if (!memcmp(buff, "numsurf", 7)) {
+      SCAN_CHECK(buff, 1, "numsurf %d", &obj->num_surfs);
+      obj->surfs = calloc(obj->num_surfs, sizeof(struct ac3d_surface_t));
+
+      for (size_t i = 0 ; i < obj->num_surfs ; ++ i) {
+        FORCE_GETS(buff, BUFF_SIZE, fp);
+
+        SCAN_CHECK(buff, 1, "SURF %d", &obj->surfs[i].tag);
+        FORCE_GETS(buff, BUFF_SIZE, fp);
+        SCAN_CHECK(buff, 1, "mat %d", &obj->surfs[i].material_idx);
+
+        FORCE_GETS(buff, BUFF_SIZE, fp);
+        SCAN_CHECK(buff, 1, "refs %d", &obj->surfs[i].refs);
+        obj->surfs[i].ref_lines = calloc(obj->surfs[i].refs, sizeof(struct ac3d_refline));
+
+        for (int j = 0 ; j < obj->surfs[i].refs ; ++ j) {
+          FORCE_GETS(buff, BUFF_SIZE, fp);
+          SCAN_CHECK(buff, 3, "%d %f %f",
+                     &obj->surfs[i].ref_lines[j].vert_idx,
+                     &obj->surfs[i].ref_lines[j].tex_x,
+                     &obj->surfs[i].ref_lines[j].tex_y);
+        }
+      }
+    } else if (!memcmp(buff, "kids", 4)) {
+      SCAN_CHECK(buff, 1, "kids %d", &obj->num_childs);
+
+      if (obj->num_childs > 0) {
+        obj->children = calloc(obj->num_childs, sizeof(struct ac3d_object_t*));
+        if (!obj->children) {
+          fprintf(stderr, "alloc failed\n");
+        }
+
+        for (size_t i = 0 ; i < obj->num_childs ; ++ i) {
+          fprintf(stderr, "%p: %d/%d\n", (void*)obj, i, obj->num_childs);
+          nameBuff[0] = '\0';
+          FORCE_GETS(buff, BUFF_SIZE, fp);
+          SCAN_CHECK(buff, 1, "OBJECT %s", nameBuff);
+          obj->children[i] = ac3d_read_obj(fp, nameBuff);
+          if (obj->children[i]) {
+            obj->children[i]->parent = obj;
+            obj->read_childs ++;
+          } else {
+            fprintf(stderr, "error reading file...\n");
+          }
+        }
+      }
+
+      return obj;
+    } else {
+      fprintf(stderr, "invalid line '%s'\n", buff);
+    }
+  }
+
+error:
+  return NULL;
 }
 
 model_t*
@@ -228,17 +339,16 @@ ac3d_load(const char *path)
 
   struct ac3d_file_t *ac3d = make_file();
   struct ac3d_object_t *obj = NULL;
+
   char buff[BUFF_SIZE];
-  while (!feof(fp)) {
-    fgets(buff, BUFF_SIZE, fp);
+  while (fgets(buff, BUFF_SIZE, fp)) {
     if (!memcmp(buff, "MATERIAL", 8)) {
       char nameBuff[BUFF_SIZE]; nameBuff[0] = '\0';
       struct ac3d_material_t mat;
-      SCAN_CHECK(buff,
+      SCAN_CHECK(buff, 15,
                  "MATERIAL %s rgb %f %f %f  "
                  "amb %f %f %f  emis %f %f %f  "
                  "spec %f %f %f  shi %d  trans %f",
-                 15,
                  nameBuff,
                  &mat.r, &mat.g, &mat.b,
                  &mat.amb_r, &mat.amb_g, &mat.amb_b,
@@ -251,92 +361,17 @@ ac3d_load(const char *path)
       PUSH_MAT(ac3d, &mat);
     } else if (!memcmp(buff, "OBJECT", 6)) {
       char nameBuff[BUFF_SIZE]; nameBuff[0] = '\0';
-      SCAN_CHECK(buff, "OBJECT %s", 1, nameBuff);
-
-      if (obj && obj->parent && obj->parent->read_childs < obj->parent->num_childs) {
-        obj->parent->children[obj->parent->read_childs] = make_object();
-        obj->parent->children[obj->parent->read_childs]->parent = obj->parent;
-        obj = obj->parent->children[obj->parent->read_childs];
-      } else if (obj == NULL) {
-        obj = make_object();
-      } else {
-        // add in file frame
-        obj = make_object();// TODO: Fix
-      }
-      obj->kind = strdup(nameBuff);
-
-      while (!feof(fp)) {
-        FORCE_GETS(buff, BUFF_SIZE, fp);
-        if (!memcmp(buff, "*name", 5)) {
-          SCAN_CHECK(buff, "*name %s", 1, nameBuff);
-        } else if (!memcmp(buff, "*data", 5)) {
-          SCAN_CHECK(buff, "*data %d", 1, &obj->data_len);
-        } else if (!memcmp(buff, "*texture", 8)) {
-          SCAN_CHECK(buff, "*texture %s", 1, nameBuff);
-          obj->texture = strdup(nameBuff);
-        } else if (!memcmp(buff, "*texrep", 7)) {
-          SCAN_CHECK(buff, "*texrep %f %f", 2, &obj->texrep_x, &obj->texrep_y);
-        } else if (!memcmp(buff, "*rot", 4)) {
-          SCAN_CHECK(buff, "*rot %f %f %f  %f %f %f  %f %f %f", 9,
-                     &obj->rot[0][0], &obj->rot[0][1], &obj->rot[0][2],
-                     &obj->rot[1][0], &obj->rot[1][1], &obj->rot[1][2],
-                     &obj->rot[2][0], &obj->rot[2][1], &obj->rot[2][2]);
-        } else if (!memcmp(buff, "*loc", 4)) {
-          SCAN_CHECK(buff,  "*loc %f %f %f", 3,
-                     &obj->pos[0], &obj->pos[1], &obj->pos[2]);
-        } else if (!memcmp(buff, "*url", 4)) {
-          SCAN_CHECK(buff, "*url %s", 1, nameBuff);
-        } else if (!memcmp(buff, "*numvert", 8)) {
-          SCAN_CHECK(buff, "*numvert %d", 1, &obj->num_verts);
-          obj->verts = calloc(obj->num_verts*3, sizeof(float));
-
-          for (size_t i = 0 ; i < obj->num_verts ; ++ i) {
-            FORCE_GETS(buff, BUFF_SIZE, fp);
-            SCAN_CHECK(buff, "%f %f %f", 3,
-                       &obj->verts[i*3], &obj->verts[i*3+1], &obj->verts[i*3+2]);
-          }
-        } else if (!memcmp(buff, "*numsurf", 8)) {
-          SCAN_CHECK(buff, "*numsurf %d", 1, &obj->num_surfs);
-          obj->surfs = calloc(obj->num_surfs, sizeof(struct ac3d_surface_t));
-
-          for (size_t i = 0 ; i < obj->num_surfs ; ++ i) {
-            FORCE_GETS(buff, BUFF_SIZE, fp);
-
-            SCAN_CHECK(buff, "SURF %d", 1, &obj->surfs[i].tag);
-            FORCE_GETS(buff, BUFF_SIZE, fp);
-            SCAN_CHECK(buff, "*mat %d", 1, &obj->surfs[i].material_idx);
-
-            FORCE_GETS(buff, BUFF_SIZE, fp);
-            SCAN_CHECK(buff, "refs %d", 1, &obj->surfs[i].refs);
-            obj->surfs[i].ref_lines = calloc(obj->surfs[i].refs, sizeof(struct ac3d_refline));
-
-            for (int j = 0 ; j < obj->surfs[i].refs ; ++ j) {
-              FORCE_GETS(buff, BUFF_SIZE, fp);
-              SCAN_CHECK(buff, "%d %f %f", 3,
-                         &obj->surfs[i].ref_lines[j].vert_idx,
-                         &obj->surfs[i].ref_lines[j].tex_x,
-                         &obj->surfs[i].ref_lines[j].tex_y);
-            }
-          }
-        } else if (!memcmp(buff, "kids", 4)) {
-          SCAN_CHECK(buff, "kids %d", 1, &obj->num_childs);
-
-          if (obj->num_childs > 0) {
-            obj->children = calloc(obj->num_childs, sizeof(struct ac3d_object_t*));            
-            // parent = obj;
-          } else {
-            //parent = obj.parent;
-          }
-
-          break; // We should exit the current object, we are done
-        }
-      }
-      if (obj->parent = NULL) {
-        PUSH_OBJ(ac3d, obj);
-      } else {
-        obj->parent->read_childs ++;
-      }
+      SCAN_CHECK(buff, 1, "OBJECT %s", nameBuff);
+      obj = ac3d_read_obj(fp, nameBuff);
+      PUSH_OBJ(ac3d, obj);
     }
+  }
+
+  if (ferror(fp)) {
+    fprintf(fp, "error reading file '%s'\n", path);
+    fclose(fp);
+    ac3d_delete(ac3d);
+    return NULL;
   }
 
   // TODO: Convert ac3d to model
