28c5c7663c476d9bd4c668c8f3adc53d75f08357
Mattias Holm <mattias.holm@openorbit.org>
2010-01-16 11:31:20 +0100

    Simple flight recorders (unfinished)

---
+++ src/avionics/flightrecorder.c
@@ -0,0 +1,73 @@
+/*
+ Copyright 2010 Mattias Holm <mattias.holm(at)openorbit.org>
+ 
+ This file is part of Open Orbit.
+ 
+ Open Orbit is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Open Orbit is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License
+ along with Open Orbit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdlib.h>
+
+#include "flightrecorder.h"
+#include "sim/spacecraft.h"
+#include <gencds/array.h>
+OOflightrecorder*
+ooNewFlightRecorder(OOspacecraft *sc, int maxSamples, int sampleInterval)
+{
+  OOflightrecorder *fr = malloc(sizeof(OOflightrecorder));
+  double_array_init(&fr->data);
+  fr->sampleInterval = sampleInterval;
+  fr->sampleTimeStamp = 0;
+  fr->maxSamples = maxSamples;
+  return fr;
+}
+
+void
+ooFlightRecorderStep(OOflightrecorder *fr, float dt)
+{
+  double currentSimTime = ooTimeGetJD();
+
+  // Time
+  double_array_push(&fr->data, currentSimTime);
+
+  // Position
+  double_array_push(&fr->data, 0.0);
+  double_array_push(&fr->data, 0.0);
+  double_array_push(&fr->data, 0.0);
+
+  // Quaternion
+  double_array_push(&fr->data, 0.0);
+  double_array_push(&fr->data, 0.0);
+  double_array_push(&fr->data, 0.0);
+  double_array_push(&fr->data, 0.0);  
+}
+
+void
+ooFlightRecorderWrite(OOflightrecorder *fr, FILE *file)
+{
+  fprintf(file, "JD, X, Y, Z, QX, QY, QZ, QW\n");
+  
+  for (size_t i = 0 ; i < fr->data.length / 8; ++ i) {
+    fprintf(file, "%f, %f, %f, %f, %f, %f, %f, %f\n",
+            fr->data.elems[i * 8 + 0],
+            
+            fr->data.elems[i * 8 + 1],
+            fr->data.elems[i * 8 + 2],
+            fr->data.elems[i * 8 + 3],
+            
+            fr->data.elems[i * 8 + 4],
+            fr->data.elems[i * 8 + 5],
+            fr->data.elems[i * 8 + 6],
+            fr->data.elems[i * 8 + 7]);
+  }
+}
---
+++ src/avionics/flightrecorder.h
@@ -0,0 +1,40 @@
+/*
+ Copyright 2010 Mattias Holm <mattias.holm(at)openorbit.org>
+ 
+ This file is part of Open Orbit.
+ 
+ Open Orbit is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Open Orbit is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License
+ along with Open Orbit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OO_FLIGHTRECORDER_H
+#define OO_FLIGHTRECORDER_H
+
+#include <gencds/array.h>
+#include "sim/simtime.h"
+#include "sim/spacecraft.h"
+
+// Records flight data in terms of:
+//   struct {double absTime; double x, y, z; double qx, qy, qz, qw; }
+
+typedef struct OOflightrecorder {
+  int sampleInterval;
+  int sampleTimeStamp;
+  int maxSamples;
+  double_array_t data;
+} OOflightrecorder;
+
+OOflightrecorder *ooNewFlightRecorder(OOspacecraft *sc, int maxSamples, int sampleInterval);
+void ooFlightRecorderStep(OOflightrecorder *fr, float dt);
+void ooFlightRecorderWrite(OOflightrecorder *fr, FILE *file);
+#endif /* !OO_FLIGHTRECORDER_H */
