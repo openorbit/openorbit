8c632aaf31d84ebb9f9692e93e80f8036e486b49
Mattias Holm <mattias.holm@openorbit.org>
2010-05-15 09:43:49 +0200

    Particle system
    Additional work on particle system, still not tested.

--- src/physics/particles.c
+++ src/physics/particles.c
@@ -1,18 +1,18 @@
 /*
  Copyright 2010 Mattias Holm <mattias.holm(at)openorbit.org>
- 
+
  This file is part of Open Orbit.
- 
+
  Open Orbit is free software: you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  Open Orbit is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Lesser General Public License for more details.
- 
+
  You should have received a copy of the GNU Lesser General Public License
  along with Open Orbit.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -27,14 +27,14 @@ static float3
 random_direction(float ra, float dec, float maxOffsetAngle)
 {
   // TODO: Generate direction with gaussian distribution?
-  
+
   // Random direction
   long dir = (random() % 1000) + 1;
   float dirf = (float)(2.0f * M_PI / (float)dir);
-  
+
   // Random offset angle in direction
   float offsetAngle = maxOffsetAngle / (float)((random() % 99) + 1);
-  
+
   return equ2cart_f(ra + offsetAngle * sinf(dirf),
                     dec + offsetAngle * cosf(dirf));
 }
@@ -45,26 +45,40 @@ plNewParticleSystem(const char *name, size_t particleCount)
 {
   PLparticles *ps = malloc(sizeof(PLparticles));
   ps->enabled = false;
+  ps->autoDisable = false;
   ps->particleCount = particleCount;
   ps->obj = NULL;
   // Note: calloc initialise all fields to 0
   ps->particles = calloc(particleCount, sizeof(PLparticle));
+
+  int_array_init(&ps->freeParticles);
+  for (int i = 0 ; i < particleCount ; ++ i) {
+    int_array_push(&ps->freeParticles, i);
+  }
+
   return ps;
 }
 
 void
-plAttachParticleSystem(PLparticles *ps, PLobject *obj)
+plSetEmitterDirection3f(PLparticles *ps, float x, float y, float z)
+{
+  ps->v = vf3_set(x, y, z);
+}
+
+void
+plAttachParticleSystem3f(PLparticles *ps, PLobject *obj, float x, float y, float z)
 {
   assert(ps != NULL);
   assert(obj != NULL);
   assert(ps->obj == NULL);
-
+  obj_array_push(&obj->psystem, ps);
   ps->obj = obj;
   if (obj->parent) {
-    obj_array_push(&obj->parent->super.sys->world->partSys, ps);
+    obj_array_push(&obj->parent->sys->world->partSys, ps);
   } else {
     obj_array_push(&obj->sys->world->partSys, ps);
   }
+  ps->p = vf3_set(x, y, z);
 }
 
 
@@ -92,12 +106,23 @@ plEnableParticleSystem(PLparticles *ps)
 }
 
 void
+plTurnOffParticleSystem(PLparticles *ps)
+{
+  assert(ps != NULL);
+  ps->autoDisable = true;
+}
+
+void
 plDisableParticleSystem(PLparticles *ps)
 {
   assert(ps != NULL);
   ps->enabled = false;
 }
 
+float rand_percent(int a)
+{
+  return ((float)(random() % a*2 - a)) * 0.01;
+}
 
 void
 plStepParticleSystem(PLparticles *ps, float dt)
@@ -105,26 +130,52 @@ plStepParticleSystem(PLparticles *ps, float dt)
   assert(ps != NULL);
   if (ps->enabled == false) return;
 
+  size_t activeParticles = 0;
   for (size_t i = 0 ; i < ps->particleCount ; ++i) {
-    ps->particles[i].p += vf3_s_mul(ps->particles[i].v, dt);
-    ps->particles[i].age += dt;
+    if (ps->particles[i].active) {
+      ps->particles[i].p += vf3_s_mul(ps->particles[i].v, dt) + vf3_s_mul(ps->obj->v, dt);
+      ps->particles[i].age += dt;
+      activeParticles++;
 
-    if (ps->particles[i].age < ps->particles[i].lifeTime) {
-
+      if (ps->particles[i].age > ps->particles[i].lifeTime) {
+        ps->particles[i].active = false;
+        int_array_push(&ps->freeParticles, i);
+        activeParticles --;
+      }
     }
   }
 
-  float newPartCount = ps->emissionRate * dt;
+  // Auto disable
+  if (ps->autoDisable) {
+    if (activeParticles == 0) {
+      ps->enabled = false;
+    }
+
+    return;
+  }
+
+  // Not off or disabled, emitt new particles
+  float newPartCount = ps->emissionRate * dt * (1.0 + rand_percent(10));
   float intPart;
   float frac = modff(newPartCount, &intPart);
   unsigned newParticles = (unsigned) intPart;
 
-  for (unsigned i = 0 ; i < newParticles ; ++i) {
-
-  }
-  // Handle remainder
+  // The fraction is handled with randomisation
   int rval = random() % 128;
-  if ((float)rval/128.0f < frac) {
+  if ((float)rval/128.0f < frac) newParticles ++;
 
+  for (unsigned i = 0 ; i < newParticles ; ++i) {
+    if (ps->freeParticles.length > 0) {
+      int i = int_array_remove(&ps->freeParticles, 0);
+      ps->particles[i].active = true;
+      ps->particles[i].age = 0.0;
+      // Adjust lifetime by +-20 %
+      ps->particles[i].lifeTime = ps->lifeTime + ps->lifeTime * rand_percent(20);
+      ps->particles[i].p = v_q_rot(ps->p, ps->obj->q);
+      ps->particles[i].v = v_q_rot(ps->v * vf3_set(rand_percent(10), rand_percent(10), rand_percent(10)), ps->obj->q);
+      ps->particles[i].rgb = ps->rgb;
+    } else {
+      break;
+    }
   }
 }
--- src/physics/particles.h
+++ src/physics/particles.h
@@ -1,18 +1,18 @@
 /*
  Copyright 2010 Mattias Holm <mattias.holm(at)openorbit.org>
- 
+
  This file is part of Open Orbit.
- 
+
  Open Orbit is free software: you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  Open Orbit is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Lesser General Public License for more details.
- 
+
  You should have received a copy of the GNU Lesser General Public License
  along with Open Orbit.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -43,30 +43,29 @@ struct PLparticles {
   //GLuint texture;
   float directionLimit;
   float3 rgb; // Default colour
-  float3 p; // Default position of emitter
+  float3 p; // Default position of emitter (relative to object position)
   float3 v; // Default velocity vector for new particle
   float emissionRate; // Emmission rate of particles
+  float lifeTime; // Average life of paticles
   size_t particleCount; // Maximum particles in system, old particles are simply
                         // deleted if they are needed.
   size_t particleMask; // Particle count mask, particle count must be power of
                        // two
   size_t oldestParticleIdx;
   size_t newestParticleIdx;
-  bool enabled;
-  //float *pos;
-  //float *colour;
-  //float *dir;
-  //float *lifeTime;
-  //float *fadeFactors;
-  
+  bool enabled; // Should we simulate the particle system at all?
+  bool autoDisable; // Do not emit new particles, and disable when no particles are alive
+
   PLparticle *particles;
+  int_array_t freeParticles;
 };
 
 
 PLparticles* plNewParticleSystem(const char *name, size_t particleCount);
 void plDeleteParticleSystem(PLparticles *ps);
 void plStepParticleSystem(PLparticles *ps, float dt);
-void plAttachParticleSystem(PLparticles *ps, PLobject *obj);
+void plAttachParticleSystem3f(PLparticles *ps, PLobject *obj, float x, float y, float z);
+void plSetEmitterDirection3f(PLparticles *ps, float x, float y, float z);
 
 void plEnableParticleSystem(PLparticles *ps);
 void plDisableParticleSystem(PLparticles *ps);
