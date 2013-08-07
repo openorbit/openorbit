8025afff93972dff63481aaab23b7343099af507
Mattias Holm <lorrden@openorbit.org>
2013-08-07 23:55:04 +0200

    Add atmosphere model (not functional) to celobject

--- src/physics/celestial-object.h
+++ src/physics/celestial-object.h
@@ -12,10 +12,12 @@
 #include <celmek/celmek.h>
 #include "physics/reftypes.h"
 #include "physics/world.h"
+#include "physics/areodynamics.h"
 
 struct pl_celobject_t {
   pl_octtree_t *tree;
   cm_orbit_t *cm_orbit;
+  pl_atmosphere_t *atm;
 };
 
 void pl_celinit(pl_world_t *world);
@@ -25,5 +27,7 @@ quatd_t pl_celobject_get_body_quat(pl_celobject_t *celobj);
 quatd_t pl_celobject_get_orbit_quat(pl_celobject_t *celobj);
 float3 pl_celobject_get_vel(pl_celobject_t *celobj);
 lwcoord_t pl_celobject_get_lwc(pl_celobject_t *celobj);
+
+double3 pl_celobject_compute_airspeed_for_object(pl_celobject_t *celobj, pl_object_t *obj);
 
 #endif
